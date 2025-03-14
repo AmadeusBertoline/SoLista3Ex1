644180b01ca9ea97d7bdd363ddb665bbd0d75607
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2025-03-14 19:22:27 -0300

    Ex 1 lista 3, dia 2

--- SOLista3ex1/src/controller/RedesController.java
+++ SOLista3ex1/src/controller/RedesController.java
@@ -1,46 +1,67 @@
 package controller;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RedesController {
 
-	// identifica e retorna o nome do Sistema Operacional
-
+	// Retorna o nome do sistema operacional
 	private String Os() {
 
 		return System.getProperty("os.name");
 
 	}
 
-	// verifica o Sistema Operacional e, de acordo com o S.O., faz a chamada de
-	// configuração de IP
+	// Retorna informações de ipv4
 
-	public void Ip(String osName) {
+	public void Ip(String osName) throws IOException {
 
 		try {
 
 			Process process = null;
 
-			if (osName.toLowerCase().contains("win")) {
+			if (osName.toLowerCase().contains("windows")) {
 
 				process = Runtime.getRuntime().exec("ipconfig");
 
 			} else if (osName.toLowerCase().contains("linux") || osName.toLowerCase().contains("mac")) {
+
 				process = Runtime.getRuntime().exec("ifconfig");
+
 			} else {
-				System.out.println("Sistema operacional não suportado.");
-				return;
+
+				System.out.println("Sistema operacional não suportado");
+
 			}
 
 			// Lê a saída do comando
-
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String linha;
+			String nomeAdaptador = "";
+			String ipv4 = "";
+
+			// Enquanto a linha que estiver sendo lida não for nula, o laço continua
+			while ((linha = reader.readLine()) != null) {
+
+				linha = linha.trim();// Remove espaços em branco no começo e no fim da string
+
+				// Identifica o nome do adaptador, se a linha possuir um
+
+				if (linha.contains("adaptador") || linha.contains("eth") || linha.contains("enp")
+						|| linha.contains("wlan")) {
+
+					nomeAdaptador = linha;
+
+				}
+
+			}
 
 		} catch (Exception e) {
+
+			e.printStackTrace();
 
 		}
 
 	}
-
 }
