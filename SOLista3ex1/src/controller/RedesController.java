8ade1ae30c55ee8c3f4f00e6c5d78335d579f900
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2025-03-17 19:05:27 -0300

    Inclusão das funções de ping e ip

--- SOLista3ex1/src/controller/RedesController.java
+++ SOLista3ex1/src/controller/RedesController.java
@@ -55,7 +55,43 @@ public class RedesController {
 
 				}
 
+				// Identifica o ipv4 na linha
+
+				if (linha.contains("IPv4") || linha.contains("inet")) {
+
+					if (linha.contains("Endereço IPv4") || linha.contains("inet")) {
+
+						ipv4 = linha.split(":")[1].trim();
+
+					} else {
+
+						if (linha.contains("inet")) {
+
+							ipv4 = linha.split(" ")[1].trim();
+
+						}
+
+					}
+
+				}
+
+				// Se encontrou tanto o nome do adaptador quanto o IPv4, imprime e reinicia para
+				// o próximo adaptador
+
+				if (!nomeAdaptador.isEmpty() && !ipv4.isEmpty()) {
+
+					System.out.println("Adapatador: " + nomeAdaptador);
+					System.out.println("IPv4: " + ipv4);
+					System.out.println();
+					nomeAdaptador = "";
+					ipv4 = "";
+
+				}
+
 			}
+
+			process.waitFor(); // espera o processo terminar
+			reader.close();
 
 		} catch (Exception e) {
 
@@ -64,4 +100,73 @@ public class RedesController {
 		}
 
 	}
+
+	// O terceiro, chamado ping, que verifica o Sistema Operacional e, de acordo com
+	// o S.O. e, faz a chamada
+	// de ping em IPv4 com 10 iterações
+
+	public void Ping(String osName) throws IOException {
+
+		try {
+
+			String enderecoIp = "www.google.com.br";
+			Process process = null;
+
+			if (osName.toLowerCase().contains("windows")) {
+
+				process = Runtime.getRuntime().exec("ping -n 10 " + enderecoIp);
+
+			} else if (osName.toLowerCase().contains("nux") || osName.toLowerCase().contains("nix")
+					|| osName.toLowerCase().contains("mac")) {
+
+				process = Runtime.getRuntime().exec("ping -c 10 " + enderecoIp);
+
+			}
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String linha;
+			String[] partes;
+
+			while ((linha = reader.readLine()) != null) {
+
+				// Exibe o tempo médio de cada teste de ping
+
+				if (linha.contains("Average")) {
+
+					partes = linha.split(" ");
+
+					for (String parte : partes) {
+
+						if (parte.contains("ms")) {
+
+							System.out.println("Tempo médio: " + parte);
+
+						}
+
+					}
+
+					if (linha.contains("rtt min/avg/max/mdev")) {
+
+						partes = linha.split(" ");
+
+						String tempoMedio = partes[3].split("/")[1];
+						System.out.println("Tempo médio de ping: " + tempoMedio + " ms");
+
+					}
+
+				}
+
+			}
+
+			process.waitFor();
+			reader.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 }
